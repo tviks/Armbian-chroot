e0c8b6cf0610e9bd56ef85448eb3b66458b4adaf
Sergey Berstnev <tviks879@gmail.com>
2020-06-28 23:46:54 +0300

    Добавление инструкции

--- depends.sh
+++ depends.sh
@@ -2,10 +2,10 @@ echo "Update package list"
 sudo apt-get update
 
 echo "Upgrade packages"
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 echo "Install tools"
-sudo apt-get install qemu qemu-user-static binfmt-support
+sudo apt-get install qemu qemu-user-static binfmt-support -y
 
 
 echo "Create directory"
